f75f04dd47e4696d1ed5845495dc7b664117c142
Araceli Arizmendi <araceli.muse@gmail.com>
2019-09-18 14:42:13 -0700

    Table views Pelicula

---
+++ Pelicula/Models/Pelicula.swift
@@ -0,0 +1,26 @@
+//
+//  Pelicula.swift
+//  Pelicula
+//
+//  Created by Alumno on 9/18/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+
+class Pelicula
+{
+    var titulo : String
+    var director : String
+    var año : Int
+    
+    init (titulo: String, director: String, año: Int)
+    {
+        self.titulo = titulo
+        self.director = director
+        self.año = año
+        
+        
+    }
+    
+}
--- Pelicula/ViewController.swift
+++ Pelicula/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var peliculas : [Pelicula] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        peliculas.append(Pelicula(titulo: "It", director: "Director 1", año: 1992))
+        peliculas.append(Pelicula(titulo: "Kill Bill", director: "Quentin Tarantino", año: 1990))
+        peliculas.append(Pelicula(titulo:"kk", director: "asdad", año: 2000 ))
     }
+  
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return peliculas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaPelicula") as?
+        CeldaPeliculaController
+        
+        celda?.lblPelicula.text = peliculas[indexPath.row].titulo
+        celda?.lblDirector.text = peliculas[indexPath.row].director
+        celda?.lblAño.text = "\(peliculas[indexPath.row].año)"
+        
+        
+         return celda!
+    }
+    
 
-
+        
+    
+    
 }
 
